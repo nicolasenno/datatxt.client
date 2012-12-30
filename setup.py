0fa8c755904f0a072a08c3069d1a9e497ff279a1
Nicola Senno <nicola.senno@redturtle.it>
2012-12-30 08:32:18 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.9.0'
+shortdesc = "python interface for SpazioDati's dataTXT and DBpedia"
+longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+tests_require = ['interlude']
+
+setup(name='datatxt.client',
+      version=version,
+      description=shortdesc,
+      long_description=longdesc,
+      classifiers = [
+            'Development Status :: 2 - Pre-Alpha',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX',
+            'Natural Language :: Italian',
+            'Natural Language :: English',
+            'Programming Language :: Python',
+            'Topic :: Database',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Text Processing :: Indexing'
+      ],
+      keywords='',
+      author='',
+      author_email='',
+      url=u'',
+      license='Python Software Foundation License',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=[],
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+            'sparql-client >= 0.13',
+            'requests >= 0.11'
+      ],
+)
